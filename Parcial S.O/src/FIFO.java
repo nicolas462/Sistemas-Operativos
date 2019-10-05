8d5417e45a3fa4b460fcc09d48477c72c2d46d57
Nicolas <Nicolas@FamiliaNicolas>
2019-10-05 10:50:39 -0500

    1

---
+++ Parcial S.O/src/FIFO.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Nicolás Espejo
+ *
+ */
+public class FIFO 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
