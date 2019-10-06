c897901e09caf64a9de8c67b74eba46a4eaae6df
Nicolas <nespejobern@uniminuto.edu.co>
2019-10-05 22:41:59 -0500

    Terminado

--- Parcial S.O/src/FIFO.java
+++ Parcial S.O/src/FIFO.java
@@ -1,16 +1,16 @@
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * Algoritmo FIFO utilizado como algoritmo de reemplazo de páginas en la 
  * planificación de acceso de datos en discos.
  * 
- * Fallo de página: Un fallo de página es la secuencia de eventos que 
- * ocurren cuando un programa intenta acceder a datos (o código) que 
- * está en su espacio de direcciones, pero que no está actualmente 
- * ubicado en la RAM del sistema.
+ * Un fallo de página es la secuencia de eventos que ocurren cuando un 
+ * programa intenta acceder a datos (o código) que está en su espacio 
+ * de direcciones, pero que no está actualmente ubicado en la RAM del sistema.
  * 
- * La emulación de este algoritmo para este código, va estar regido
- * por una co
+ * La emulación de este algoritmo, para este código, va estar regido
+ * por un valor máximo de cinco procesos.
  */
 
 /**
@@ -21,16 +21,23 @@ public class FIFO
 {	
 	public static void main(String[] args) 
 	{
-		Funciones fn = new Funciones();
-		LinkedList cola = new LinkedList(); //poll, peek, offer
-		cola.offer(1);
-		cola.offer(2);
-		cola.offer(3);
+		try
+		{
+			BufferedReader br = new BufferedReader (new InputStreamReader ( System.in));
+			int x = 0;
+			Funciones fn = new Funciones();
+			
+			System.out.println("En cualquier momento, ingrese '-1' para acabar el programa.");
+			while (true)
+			{
+				System.out.print("Ingrese el número de proceso a añadir: ");
+				x = Integer.parseInt(br.readLine());
+				if (x == -1)
+					break;
+				fn.add(x);
+			}	
+		} catch (Exception ex) {}
 		
-		/*for (int i= 0; i < 3 ; i++) {
-			System.out.println(cola.peek());
-		cola.poll();}*/
-		System.out.println(cola.size());
 	}
 	
 }
